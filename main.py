3f6ca2be5ca477398224da3d9c2e918e127b98df
GodinhoRaphael <127311037+GodinhoRaphael@users.noreply.github.com>
2024-05-15 22:37:47 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-def converter_lista_para_dict(lista_de_pares):
-  dicionario = dict(lista_de_pares)
-  return dicionario
+itens = [
+  {"nome": "caneta", "valor": 70.0, "estoque": 200},
+  {"nome": "lápis", "valor": 0.80, "estoque": 47},
+  {"nome": "caderno", "valor": 80.0, "estoque": 100},
+  {"nome": "borracha", "valor": 1.20, "estoque": 60}
+]
 
-# Exemplo de uso:
-lista_notas_alunos = [("Ana", 8), ("João", 7), ("Maria", 9)]
-dicionario_notas_alunos = converter_lista_para_dict(lista_notas_alunos)
-print(dicionario_notas_alunos)
+itens_em_promocao = []
+
+for item in itens:
+  if item["valor"] > 60.0 and item["estoque"] > 50:
+      novo_valor = item["valor"] * 0.8
+      item_em_promocao = {
+          "nome": item["nome"],
+          "valor": (item["valor"], novo_valor),
+          "estoque": item["estoque"]
+      }
+      itens_em_promocao.append(item_em_promocao)
+
+for item in itens_em_promocao:
+  print(item)
